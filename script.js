fcbf426c9acd042fa5668192c737e6fe147e0b74
sojin-coder <sojinprak@gmail.com>
2026-03-24 01:08:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+
+// Loading
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector(".loading-screen").style.display = "none";
+  }, 1000);
+};
+
+// Firebase imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+// Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBN9CPViV4ok9kAhZTBGm8h76x8XcAdmGk",
+  authDomain: "my-listing-947ff.firebaseapp.com",
+  projectId: "my-listing-947ff",
+  storageBucket: "my-listing-947ff.appspot.com",
+  messagingSenderId: "1039885458733",
+  appId: "1:1039885458733:web:0e3a06b5dd600e3b2f07a4"
+};
+
+// Init
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// DOM
+const tbl = document.getElementById("tbl");
+const form = document.getElementById("form");
+
+// SHOW DATA
+onSnapshot(collection(db, "students"), (snapshot) => {
+  let html = "";
+  let i = 1;
+
+  snapshot.forEach((docSnap) => {
+    const d = docSnap.data();
+
+    html += `
+      <tr>
+        <td>${i++}</td>
+        <td>${d.full_name}</td>
+        <td>${d.address}</td>
+        <td>${d.money}</td>
+        <td>
+          <button class="btn btn-info btn-sm" onclick="edit('${docSnap.id}')">Edit</button>
+          <button class="btn btn-danger btn-sm" onclick="del('${docSnap.id}')">Delete</button>
+        </td>
+      </tr>
+    `;
+  });
+
+  tbl.innerHTML = html;
+});
+
+// ADD
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = Object.fromEntries(new FormData(form));
+
+  await addDoc(collection(db, "students"), data);
+
+  Swal.fire("Success", "Added!", "success");
+  form.reset();
+});
+
+// DELETE
+window.del = async (id) => {
+  await deleteDoc(doc(db, "students", id));
+  Swal.fire("Deleted", "", "success");
+};
+
+// EDIT
+window.edit = async (id) => {
+  const snap = await getDoc(doc(db, "students", id));
+  const d = snap.data();
+
+  document.getElementById("u_id").value = id;
+  document.getElementById("u_name").value = d.full_name;
+  document.getElementById("u_address").value = d.address;
+  document.getElementById("u_money").value = d.money;
+
+  new bootstrap.Modal(document.getElementById("modal")).show();
+};
+
+// UPDATE
+document.getElementById("form-update").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const id = document.getElementById("u_id").value;
+
+  await updateDoc(doc(db, "students", id), {
+    full_name: document.getElementById("u_name").value,
+    address: document.getElementById("u_address").value,
+    money: document.getElementById("u_money").value,
+  });
+
+  Swal.fire("Updated", "", "success");
+
+  bootstrap.Modal.getInstance(document.getElementById("modal")).hide();
+});
